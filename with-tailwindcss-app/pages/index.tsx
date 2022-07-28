5dfd6f48c64e40d70b10c7a0d726a881cfb6a709
curryaab <aaapcgamers@gmail.com>
2022-07-28 15:21:14 -0700

    install tailwind and next project

---
+++ with-tailwindcss-app/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import { global } from 'styled-jsx/css'
+const Home: NextPage = () => {
+  return (
+   <div>
+      hhh
+   </div>
+  )
+}
+
+export default Home
